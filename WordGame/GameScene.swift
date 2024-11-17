45ec21f1932797bf196f1045ef7910db2e99eaeb
Phil Werner <philwerner@shaw.ca>
2024-11-16 20:50:22 -0800

    Changed letters in alphabet, added much more vowels and common letters

--- WordGame/GameScene.swift
+++ WordGame/GameScene.swift
@@ -41,8 +41,14 @@ class GameScene: SKScene {
     }
     
     func pickLetter() -> String {
-        let pick = arc4random_uniform(26)
-        let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        
+        let alphabet = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"]
+        
+ //       print("alphabet count = ", alphabet.count)
+        
+        let tempNum = alphabet.count - 1
+        var numLettersInAlphabet = UInt32(tempNum)
+        let pick = arc4random_uniform(numLettersInAlphabet)
         
         return alphabet[Int(pick)]
     }
@@ -217,10 +223,10 @@ class GameScene: SKScene {
             }
         }
 //
-        arrayOfLetters[0][0] = "Z"
-        arrayOfLetters[0][1] = "I"
-        arrayOfLetters[0][2] = "P"
-        arrayOfLetters[0][3] = "S"
+   //     arrayOfLetters[0][0] = "Z"
+  //      arrayOfLetters[0][1] = "I"
+ //       arrayOfLetters[0][2] = "P"
+ //       arrayOfLetters[0][3] = "S"
  //       arrayOfLetters[0][4] = "K"
 //        arrayOfLetters[1][0] = pickLetter()
 //        arrayOfLetters[1][1] = pickLetter()
@@ -1969,7 +1975,7 @@ class GameScene: SKScene {
             })
         }
         
-        var lowerCaseWord = chosenWord.lowercased()
+        let lowerCaseWord = chosenWord.lowercased()
         
         if (isNotUsedBefore(word: lowerCaseWord)) {
             print("word was used before")
